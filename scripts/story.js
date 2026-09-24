c63a7d1e037bef60b4aefde78e819402b3f87604
CryptidSmile <cryptd.smile@gmail.com>
2026-09-24 12:31:16 -0500

    Editing dialogue, added some placeholders for now

--- scripts/story.js
+++ scripts/story.js
@@ -1,6 +1,10 @@
 // Stores the story data
 
 const story = [
+  { speaker: "Narrator", text: "Once upon a time, in a land far away" },
+  { speaker: "Narrator", text: "There lived a detective in a crime riddled city called Crimopolis"},
+  { speaker:"Player", text:"This person has been arrested on suspicions of a crime. How do I get the truth out of them?" },
+  { speaker:"Narrator", text:"What will you do?" },
   { speaker: "Player", text: "How did you know the victim?" },
   { speaker: "Suspect", text: "They were my next door neighbor, and we occasionally ran into each other in the neighborhood." },
   { speaker: "Player", text: "Are you sure that's all you knew them by?" },
